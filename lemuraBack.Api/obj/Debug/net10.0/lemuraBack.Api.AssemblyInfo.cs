6bdf4b9882c281d020d33f999c246c4d69e4474d
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2026-02-02 17:47:39 +0100

    runServer

--- lemuraBack.Api/obj/Debug/net10.0/lemuraBack.Api.AssemblyInfo.cs
+++ lemuraBack.Api/obj/Debug/net10.0/lemuraBack.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lemuraBack.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096907515581675aa9ea6834619cd96b4e04df75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b820303d38085b1592857c795366f9499820effc")]
 [assembly: System.Reflection.AssemblyProductAttribute("lemuraBack.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lemuraBack.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
